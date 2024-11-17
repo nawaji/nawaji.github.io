0ecc45bdb740dca49ce0d6ac9942b5905d32e296
nawaji <Jonathant15191@gmail.com>
2024-11-17 03:24:01 -0800

    Project start using create-react-app
    Starter files from create-react-app

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import './styles/App.css';
+
+function App() {
+  return (
+    <html>
+      <head>
+        <title>
+          Jonathan To
+        </title>
+      </head>
+      <body>
+        <header>
+          <nav>
+            <ul className='navitem'>
+              <li>Home</li>
+              <li>About</li>
+              <li>Projects</li>
+            </ul>
+          </nav>
+        </header>
+        <div>
+          <h1>Jonathan To</h1>
+          <h3>Software Engineering</h3>
+        </div>
+        <div>
+          <h2>About</h2>
+        </div>
+        <div>
+          <p>
+            Lorem ipsum odor amet, consectetuer adipiscing elit. 
+            Nec gravida a torquent maximus ante sapien.
+          </p>
+        </div>
+        <div>
+          <h2>Projects</h2>
+        </div>
+        <footer>
+          Github, LinkedIn Links
+        </footer>
+      </body>
+    </html>
+  );
+}
+
+export default App;
