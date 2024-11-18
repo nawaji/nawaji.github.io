3f784f7de9af57034597fe7febef19a399712732
nawaji <Jonathant15191@gmail.com>
2024-11-18 02:52:38 -0800

    Page layout adjustments
    Continuing to layout the elements of the web page. For now it'll all be in App.js, but I want to split it into components later.

--- src/App.js
+++ src/App.js
@@ -1,42 +1,55 @@
 import './styles/App.css';
+import profile from './images/profile.png'
 
 function App() {
   return (
     <html>
       <head>
+      <script src="https://cdn.jsdelivr.net/npm/handlebars@latest/dist/handlebars.js">
+      </script>
         <title>
           Jonathan To
         </title>
       </head>
       <body>
-        <header>
-          <nav>
-            <ul className='navitem'>
-              <li>Home</li>
-              <li>About</li>
-              <li>Projects</li>
-            </ul>
+        <div className='centered-content'>
+        <header className='container'>
+          <nav className='navbar'>
+            <div class='nav-logo'><a href='#'>Logo</a></div>
+              <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Projects</a></li>
+              </ul>
           </nav>
         </header>
-        <div>
-          <h1>Jonathan To</h1>
-          <h3>Software Engineering</h3>
+        <div className='home'>
+          <h1 className='home-name'>Jonathan To</h1>
+          <h3 className='home-caption'>Software Engineering</h3>
         </div>
         <div>
-          <h2>About</h2>
+          <div className='divider'>About</div>
         </div>
-        <div>
+        <div className='container'>
+          <div className='about-text'>
           <p>
             Lorem ipsum odor amet, consectetuer adipiscing elit. 
             Nec gravida a torquent maximus ante sapien.
           </p>
+          </div>
+          <div className='about-picture-frame'>
+            <img src={profile} alt='profile picture' className='about-picture' />
+          </div>
         </div>
         <div>
-          <h2>Projects</h2>
+        <div className='divider'>Projects</div>
         </div>
         <footer>
-          Github, LinkedIn Links
+          <div className='divider'>
+            Github, LinkedIn Links
+          </div>
         </footer>
+        </div>
       </body>
     </html>
   );
