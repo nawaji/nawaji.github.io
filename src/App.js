dcba85e059c911d61ebefaabce3a64d2e58323c8
nawaji <jonathant15191@gmail.com>
2024-11-21 22:51:07 -0800

    more html & css work

--- src/App.js
+++ src/App.js
@@ -24,32 +24,33 @@ function App() {
           </nav>
         </header>
         <div className='home'>
-          <h1 className='home-name'>Jonathan To</h1>
-          <h3 className='home-caption'>Software Engineering</h3>
+          <p className='home-name'>Jonathan To</p>
+          <p className='home-caption'>Software Engineering</p>
         </div>
-        <div>
-          <div className='divider'>About</div>
-        </div>
+        <div className='divider'>About</div>
         <div className='container'>
-          <div className='about-text'>
-          <p>
-            Lorem ipsum odor amet, consectetuer adipiscing elit. 
-            Nec gravida a torquent maximus ante sapien.
-          </p>
+          <div className='about'>
+            <div className='about-text'>
+           <p>
+             Hello! My name is Jonathan. I graduated from Oregon State University
+             in 2024 with a Bachelor's Degree in Computer Science, with a focus on Security.
+           </p>
           </div>
-          <div className='about-picture-frame'>
-            <img src={profile} alt='profile picture' className='about-picture' />
+           <div className='about-picture-frame'>
+             <img src={profile} alt='profile picture' className='about-picture' />
+           </div>
           </div>
         </div>
-        <div>
         <div className='divider'>Projects</div>
-        </div>
-        <footer>
-          <div className='divider'>
-            Github, LinkedIn Links
+          <div className='container'>
+            <div className='project-list'>
+              <div className='project-card'>
+                <p>test</p>
+              </div>
+            </div>
           </div>
-        </footer>
         </div>
+
       </body>
     </html>
   );
