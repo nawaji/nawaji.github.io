28b0d4fba1357901cbca574079d5216e515ea8a6
nawaji <Jonathant15191@gmail.com>
2024-11-23 23:26:58 -0800

    Added more to 'About' and new font
    + Overused Grotesk font (https://github.com/RandomMaerks/Overused-Grotesk)
    + More text to About section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './styles/App.css';
-import profile from './images/profile.png'
+import profile from './images/profile.jfif'
+import project_img from './images/soundbendor.png'
 
 function App() {
   return (
     <html>
       <head>
-      <script src="https://cdn.jsdelivr.net/npm/handlebars@latest/dist/handlebars.js">
-      </script>
+      <script src="https://cdn.jsdelivr.net/npm/handlebars@latest/dist/handlebars.js" />
         <title>
           Jonathan To
         </title>
@@ -17,7 +17,6 @@ function App() {
           <nav className='navbar'>
             <div class='nav-logo'><a href='#'>Logo</a></div>
               <ul>
-                <li><a href="#">Home</a></li>
                 <li><a href="#">About</a></li>
                 <li><a href="#">Projects</a></li>
               </ul>
@@ -31,23 +30,31 @@ function App() {
         <div className='container'>
           <div className='about'>
             <div className='about-text'>
-           <p>
-             Hello! My name is Jonathan. I graduated from Oregon State University
-             in 2024 with a Bachelor's Degree in Computer Science, with a focus on Security.
-           </p>
-          </div>
-           <div className='about-picture-frame'>
-             <img src={profile} alt='profile picture' className='about-picture' />
-           </div>
+              <p>
+                Hello! My name is Jonathan. I graduated from Oregon State University
+                in 2024 with a Bachelor's Degree in Computer Science, with a focus on Security. 
+              </p>
+              <p>
+                Initially, I attended Oregon State to study Mechanical Engineering.
+                Learning MATLAB during my Sophomore year made me realize I would enjoy studying
+                anything computer and software related. More specifically, security related topics
+                such as networking and malware analysis, but also web design.
+
+              </p>
+              <p>
+                My hobbies include graphic design / design, video games, and photography. 
+              </p>
+            </div>
+            <img src={profile} alt='selfie' className='about-picture' />
           </div>
         </div>
         <div className='divider'>Projects</div>
           <div className='container'>
             <div className='project-list'>
               <div className='project-card'>
-                <img src={profile} alt='project'/>
+                <img src={project_img} alt='project'/>
                 <div className='project-title'>Soundbendor Website</div>
-                <ul>
+                <ul className='project-links'>
                   <li><a href='#'>Link</a></li>
                   <li><a href='#'>Github</a></li>
                 </ul>
@@ -55,7 +62,6 @@ function App() {
             </div>
           </div>
         </div>
-
       </body>
     </html>
   );
