97c3a209fdcf4b83b3f5f5ee507a57426b41547e
nawaji <Jonathant15191@gmail.com>
2024-11-24 17:43:59 -0800

    fix unmet dependencies error during build

--- src/components/nav_bar.js
+++ src/components/nav_bar.js
@@ -2,7 +2,7 @@ const nav_bar = () => {
     return (
         <header className='container'>
           <nav className='navbar'>
-            <div class='nav-logo'><a href='#'>JT</a></div>
+            <div class='nav-logo'>JT</div>
               <ul>
               <a href="#about"><li className='int-button'>About</li></a>
               <a href="#projects"><li className='int-button'>Projects</li></a>
